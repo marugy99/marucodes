20b1b5f2fc677f101af2aba09d751d5686907989
marugy99 <marugy99@gmail.com>
2021-08-30 22:33:13 -0400

    Added arrow to Projects title and classes

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,27 +2,32 @@ import client from "../../client";
 import BlockContent from "@sanity/block-content-to-react"
 import { checkObj } from "../../functions/coolFunctions";
 import Head from "../../components/Head";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 const Projects = ({ projectsData }) => {
     return ( 
-        <section>
+        <section id="projects">
             <Head title="Projects"/>
-            <h1>My projects</h1>
+            <header>
+                <MdKeyboardArrowRight /><h1>Projects</h1>
+            </header>
             {
                 projectsData.map((project, index) => (
-                    <article key={index}>
-                        <h2>{project.title}</h2>
-                        <BlockContent blocks={project.description} projectId="ewz4ezcb" dataset="production" />
-                        <a href={project.link} rel="noreferrer" target="_blank">Live site</a>
-                        <br />
-                        <a href={project.repo} rel="noreferrer" target="_blank">Repository</a>
-                        {
-                            project.stack.map((proj, index) => (
-                                <ul key={index}>
-                                    <li>{proj}</li>
-                                </ul>
-                            ))
-                        }
+                    <article key={index} className="project-tile">
+                        <div>
+                            <h2>{project.title}</h2>
+                            <BlockContent blocks={project.description} projectId="ewz4ezcb" dataset="production" />
+                            <a href={project.link} rel="noreferrer" target="_blank">Live site</a>
+                            <br />
+                            <a href={project.repo} rel="noreferrer" target="_blank">Repository</a>
+                            {
+                                project.stack.map((proj, index) => (
+                                    <ul key={index}>
+                                        <li>{proj}</li>
+                                    </ul>
+                                ))
+                            }
+                        </div>
                         <img src={project.mainImage.asset.url} alt={project.mainImage.alt} />
                     </article>
                 ))
