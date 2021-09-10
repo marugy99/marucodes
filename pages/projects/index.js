dd3f52ea7c515b158c32f7a3b15c4866f3c1804c
marugy99 <marugy99@gmail.com>
2021-09-09 21:53:03 -0300

    Moved stack list below project title

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,7 +2,7 @@ import client from "../../client";
 import BlockContent from "@sanity/block-content-to-react"
 import { checkObj } from "../../functions/coolFunctions";
 import Head from "../../components/Head";
-import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdKeyboardArrowRight, MdLaunch } from "react-icons/md";
 
 const Projects = ({ projectsData }) => {
     return ( 
@@ -11,22 +11,24 @@ const Projects = ({ projectsData }) => {
             <header>
                 <MdKeyboardArrowRight /><h1>Projects</h1>
             </header>
+            <p>These are some of the projects I’ve built while learning new things.</p>
             {
                 projectsData.map((project, index) => (
                     <article key={index} className="project-tile">
                         <div>
                             <h2>{project.title}</h2>
+                            <ul>
+                                {
+                                    project.stack.map((proj, index) => (
+                                            <li key={index}>{proj}</li>
+                                            ))
+                                }
+                            </ul>
                             <BlockContent blocks={project.description} projectId="ewz4ezcb" dataset="production" />
-                            <a href={project.link} rel="noreferrer" target="_blank">Live site</a>
-                            <br />
-                            <a href={project.repo} rel="noreferrer" target="_blank">Repository</a>
-                            {
-                                project.stack.map((proj, index) => (
-                                    <ul key={index}>
-                                        <li>{proj}</li>
-                                    </ul>
-                                ))
-                            }
+                            <div className="project-links">
+                                <div><MdLaunch /><a href={project.link} rel="noreferrer" target="_blank">Live site</a></div>
+                                <a href={project.repo} rel="noreferrer" target="_blank">Repository</a>
+                            </div>
                         </div>
                         <img src={project.mainImage.asset.url} alt={project.mainImage.alt} />
                     </article>
