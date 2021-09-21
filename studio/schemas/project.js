bfef5232bf36d6f8239fb53a4393a1397dfe4659
marugy99 <marugy99@gmail.com>
2021-09-20 23:00:34 -0300

    Added extract field to post schema

--- studio/schemas/project.js
+++ studio/schemas/project.js
@@ -1,39 +1,38 @@
 export default {
-    name: "project",
-    title: "Project",
-    type: "document",
-    fields: [
-        {
-            name: "title",
-            type: "string"
-        },
-        {
-            name: "description",
-            title: "Description",
-            type: "blockContent"
-        },
-        {
-            name: "link",
-            type: "string"
-        },
-        {
-            name: "repo",
-            type: "string"
-        },
-        {
-            name: 'stack',
-            title: 'Stack',
-            type: 'array',
-            of: [{type: 'string'}],
-            
-        },
-        {
-            name: 'mainImage',
-            title: 'Main image',
-            type: 'image',
-            options: {
-              hotspot: true,
-            },
-        }
-    ]
-}
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "blockContent",
+    },
+    {
+      name: "link",
+      type: "string",
+    },
+    {
+      name: "repo",
+      type: "string",
+    },
+    {
+      name: "stack",
+      title: "Stack",
+      type: "array",
+      of: [{ type: "string" }],
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
