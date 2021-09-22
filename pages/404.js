9498a7451a6e149f5aa8791da3ffdfe8333384e5
marugy99 <marugy99@gmail.com>
2021-09-21 22:58:48 -0300

    Custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Head from "../components/Head";
+
+export default function Custom404() {
+  return (
+    <section className="content">
+      <Head title="Not Found" />
+      <h1>404 - Page Not Found</h1>
+    </section>
+  );
+}
