f692d98852e51538d5549be234822944e90e6d37
marugy99 <marugy99@gmail.com>
2021-09-24 18:54:33 -0300

    Open graph tags

--- components/Head.js
+++ components/Head.js
@@ -6,6 +6,25 @@ const Header = ({ title }) => {
       <title>{title} | Call me Maru</title>
       <meta name="description" content="Personal website" />
       <link rel="icon" href="/favicon.png" />
+      <meta property="og:title" content="Call me Maru" />
+      <meta
+        property="og:description"
+        content="Self-taught front-end developer who is passionate about learning new technologies and building things."
+      />
+      <meta property="og:url" content="https://marucodes.com/" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:image"
+        content="https://i.ibb.co/pzQbv1H/marucodes.png"
+      />
+      <meta property="og:image:width" content="1280" />
+      <meta property="og:image:height" content="640" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:image"
+        content="https://i.ibb.co/pzQbv1H/marucodes.png"
+      />
+      <meta property="twitter:site" content="@marulucenat" />
     </Head>
   );
 };
